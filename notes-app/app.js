6f6ce950f5994a986685c18fbbf71ecb691cb2e7
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-10-04 16:41:08 +0200

    cleaning up app.js a bit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,12 @@
-// need to provide relative path - with ./ you can go to the folder where you are at and then access the file.
 // const add = require('./utils.js');
 // const sum = add(2, 3);
-
-// here you run the things from this file.
 // console.log(sum);
 
-const message = require('./notes.js');
+const getNotes = require('./notes.js');
 const chalk = require('chalk');
-
 // const validator = require('validator');
 
-console.log(message());
+console.log(getNotes());
 // console.log(validator.isEmail('ieva@example.com'));
 // console.log(validator.isURL('https://example.com'))
 console.log(chalk.green.bold.inverse('success!'))
