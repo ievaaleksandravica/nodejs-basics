349457b25ec5b2683f6e8bd12b368758e1aef47b
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-10-05 11:42:06 +0200

    process.argv function to read commands from terminal

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ This repository is based on the Udemy course  [The Complete Node.js Developer Co
    * Script to call functions from other files 
    * Require and use validator npm module
    * Require and use chalk npm module
+   * Get the arguments entered in console. e.g. `node app.js add`  `process.argv` - argument vector (array) - first node path executable on your machine, path to app.js, value you provided
 * `notes.txt` File created as a result of the above mentioned file. Appended by running script from ch-1-append-file.js
 * `notes.js` Functions to be called in app.js file
 
--- notes-app/app.js
+++ notes-app/app.js
@@ -6,7 +6,19 @@ const getNotes = require('./notes.js');
 const chalk = require('chalk');
 // const validator = require('validator');
 
-console.log(getNotes());
+// console.log(getNotes());
 // console.log(validator.isEmail('ieva@example.com'));
 // console.log(validator.isURL('https://example.com'))
-console.log(chalk.green.bold.inverse('important!'))
+// console.log(chalk.green.bold.inverse('important!'))
+
+console.log(process.argv);
+
+const command = process.argv[2];
+
+if (command === "add") {
+    console.log('Executing add function..')
+} else if (command === "remove") {
+    console.log('Executing remove function..')
+} else {
+    console.log('Sorry, I do not know this function..')
+}
