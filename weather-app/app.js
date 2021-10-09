2dc8de144f5e202e5f5c17120022f74ecfdedb3d
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-10-09 13:45:47 +0200

    weather api setup and postman request npm package

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,19 @@
-console.log('Starting..');
+const request = require('postman-request');
+const config = require('../config.js');
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000);
+const weatherAPI = config.keys.WEATHERSTACK_KEY;
+const url = `http://api.weatherstack.com/current?access_key=${weatherAPI}&query=37.8267,-122.4233`
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0);
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+});
 
-console.log('Stopping..');
+// console.log('Starting..');
+// setTimeout(() => {
+//     console.log('2 second timer')
+// }, 2000);
+// setTimeout(() => {
+//     console.log('0 second timer')
+// }, 0);
+// console.log('Stopping..');
