56064673a24b0edfebfdc8bc8741b14b1985bd80
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-10-28 15:06:49 +0200

    heroku adjustments

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,6 +10,7 @@ const forecast = require('./utils/forecast.js')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -108,7 +109,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
