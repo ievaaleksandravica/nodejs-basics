a781b7a5056e5cd1bed2b9c538e56b1bb86c87bc
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-11-05 17:40:16 +0100

    async await

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -14,3 +14,17 @@ User.findByIdAndUpdate('6185121b2796838599d8514a', {
 }).catch((error) => {
     console.log(error)
 })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age: age })
+    const count = await User.countDocuments({ age: age })
+    return { user, count }
+}
+
+updateAgeAndCount('6183aa898f14a248e3eeea23', 20)
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
