abd6bfa42133077565d3498279ce0442aecbcf5f
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-11-05 12:37:57 +0100

    POST request for Tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,8 +17,17 @@ app.post('/users', (req, res) => {
         console.log(error)
         res.status('400').send(error)
     })
-
 })
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then((response) => {
+        res.send(response)
+    }).catch((error) => {
+        res.status("400").send(error)
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}.`)
