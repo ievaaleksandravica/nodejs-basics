1872b1a6a2816e4d98840d5fbb4762d3dbc932b6
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-11-09 14:51:48 +0100

    adjusting user model and router to setup mongoose middleware

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,43 +1,59 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const bcrypt = require('bcrypt')
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number.')
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number.')
+                }
             }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email address is not valid.')
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email address is not valid.')
+                }
             }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Passoword cannot contain word "password"')
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            trim: true,
+            validate(value) {
+                if (value.toLowerCase().includes('password')) {
+                    throw new Error('Passoword cannot contain word "password"')
+                }
             }
         }
     }
+)
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    console.log('Just before saving..')
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
 })
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
