7bd10f1615f014a1bbf60d8661cd5234917c27a6
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-11-13 11:00:37 +0100

    customizing user object

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -65,6 +65,15 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user
 }
 
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
+
 // generate JWT for user
 userSchema.methods.generateAuthToken = async function () {
     const user = this
